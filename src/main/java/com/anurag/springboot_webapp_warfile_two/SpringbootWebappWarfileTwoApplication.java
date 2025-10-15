e7b08ca8f85052248770895f77953cf770f79d5c
Anurag-Kumar-Poddar <anuragkumarpoddar736@gmail.com>
2025-10-15 13:04:20 +0530

    all files updated : commit - 1

---
+++ src/main/java/com/anurag/springboot_webapp_warfile_two/SpringbootWebappWarfileTwoApplication.java
@@ -0,0 +1,21 @@
+package com.anurag.springboot_webapp_warfile_two;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringbootWebappWarfileTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebappWarfileTwoApplication.class, args);
+	}
+	
+	@RequestMapping("/hii")
+	public String hii() {
+		return "<h1>wlcome to tomcat! webapp 2</h1>";
+	}
+
+}
---
+++ src/test/java/com/anurag/springboot_webapp_warfile_two/SpringbootWebappWarfileTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anurag.springboot_webapp_warfile_two;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebappWarfileTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
